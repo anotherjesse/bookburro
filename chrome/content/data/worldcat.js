fe38b387596f3d84da1cec346d415a2b44386770
Jesse Andrews <anotherjesse@gmail.com>
2009-10-02 18:30:52 -0700

    add license block to data  source files

--- chrome/content/data/worldcat.js
+++ chrome/content/data/worldcat.js
@@ -1,3 +1,17 @@
+/*
+* Copyright 2005-2009 Jesse Andrews
+*
+* This file may be used under the terms of of the
+* GNU General Public License Version 3 or later (the "GPL"),
+* http://www.gnu.org/licenses/gpl.html
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+* for the specific language governing rights and limitations under the
+* License.
+*/
+
+
 var BB = BB || {};
 
 BB.worldcat = function(location) {
